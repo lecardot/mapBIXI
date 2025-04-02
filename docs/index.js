b7f07cb1469a95d47a86fefcb2cb560d484a2ead
lecardot <lecardo.t.14@gmail.com>
2025-04-02 10:10:44 -0400

    Add init map

--- docs/index.js
+++ docs/index.js
@@ -4,6 +4,9 @@ async function renderMap() {
     L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
     L.control.scale().addTo(map);
 
+    var pointYUL = new L.LatLng(45.4680288, -73.7276389);
+    map.setView(pointYUL, 6);
+
     await fetch(`https://gbfs.velobixi.com/gbfs/fr/station_information.json`)
         .then(res => res.json())
         .then(res => {
