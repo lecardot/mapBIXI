2618eda89342ef62b34c571e24b6b3b2cd621891
lecardot <lecardo.t.14@gmail.com>
2025-04-02 10:39:24 -0400

    Add tooltips

--- docs/index.js
+++ docs/index.js
@@ -1,3 +1,20 @@
+
+function distance(a, b) {
+    return 1000;
+}
+
+function clickAction(e, stations)
+{
+    for (var station of stations) {
+        if (distance({lat: e.lat, lon: e.lon}, {lat: station.lat, lon: station.lon}) < 1000) {
+
+        } else {
+
+        }
+    }
+}
+
+
 async function renderMap() {
     const map = L.map(document.querySelector(".map"), { 'worldCopyJump': true });
 
@@ -5,13 +22,15 @@ async function renderMap() {
     L.control.scale().addTo(map);
 
     var pointYUL = new L.LatLng(45.4680288, -73.7276389);
-    map.setView(pointYUL, 6);
+    map.setView(pointYUL, 8);
 
     await fetch(`https://gbfs.velobixi.com/gbfs/fr/station_information.json`)
         .then(res => res.json())
-        .then(res => {
-            for (var station of res["data"]["stations"]) {
-                L.marker([station.lat, station.lon]).addTo(map);
+        .then(res => res["data"]["stations"])
+        .then(stations => {
+            for (var station of stations) {
+                let latlng = L.marker([station.lat, station.lon]).addTo(map); // .on('click', (e) => clickAction(e, stations)).addTo(map);
+                L.tooltip(latlng, {content: station.name}).addTo(map);
             }
         })
 }
