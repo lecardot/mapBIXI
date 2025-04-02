5dec7c914c4ac65db895618d0e5868e4f4792bac
Théo.L <lecardot14@gmail.com>
2025-04-02 07:58:43 -0400

    Init Commit

---
+++ docs/index.js
@@ -0,0 +1,16 @@
+async function renderMap() {
+    const map = L.map(document.querySelector(".map"), { 'worldCopyJump': true });
+
+    L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png").addTo(map);
+    L.control.scale().addTo(map);
+
+    await fetch(`https://gbfs.velobixi.com/gbfs/fr/station_information.json`)
+        .then(res => res.json())
+        .then(res => {
+            for (var station of res["data"]["stations"]) {
+                L.LatLng(station.lat, station.lon).addTo(map);
+            }
+        })
+}
+
+renderMap().catch(console.error)
